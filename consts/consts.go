f7906f36979c7bf6ad5abe42aed9ac188924d3c3
chethraj <cheth12228@gmail.com>
2024-08-23 21:00:07 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "AvalonX""
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "AVXH"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = " AVX"
 )
 
 var ID ids.ID
